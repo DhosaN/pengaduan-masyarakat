be0d98dd67a83c79a556ce3025fea46b89aab894
gnendraaw <itsgnendraaw@gmail.com>
2023-01-19 13:51:58 +0800

    add store aduan data logic

--- app/controllers/Pengaduan.php
+++ app/controllers/Pengaduan.php
@@ -8,8 +8,23 @@ class Pengaduan extends Controller {
         $this->view('templates/footer');
     }
 
-    public function store($data)
+    public function store()
     {
+        $data = [
+            'nama' => $_POST['nama'],
+            'email' => $_POST['email'],
+            'aduan' => $_POST['aduan'],
+        ];
 
+        if ($this->model('aduan_model')->addAduan($data) > 0)
+        {
+            $_SESSION['success']['msg'] = 'laporan berhasil terkirim!';
+            header('location: ' . BASE_URL);
+        }
+        else 
+        {
+            $_SESSION['error']['msg'] = 'terjadi kesalahan!';
+            header('location: ' . BASE_URL . '/pengaduan');
+        }
     }
 }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -8,7 +8,7 @@ class Controller {
 
     public function model($model) 
     {
-        require_once "../app/views/{$model}.php";
+        require_once "../app/models/{$model}.php";
         return new $model;
     }
 }
